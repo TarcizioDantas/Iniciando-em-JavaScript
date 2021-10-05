33fd6c16f6c9aa1d1e0c17390d931f35ae9a09d0
Tarcizio Dantas <tarciziomd@gmail.com>
2021-10-05 11:59:05 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
-var nome = "Tarcizio Dantas"
+var lista = ["maçã","pêra","laranja"];
+//lista.push("uva");
+//lista.pop();
+console.log(lista.length);
+
+
+/*var nome = "Tarcizio Dantas"
 var n1 = 20;
 var n2 = 30;
 var frase = "Japão é o melhor time do mundo";
-//alert("Bem Vindo! "+ nome);
-//alert(idade + idade2);
+alert("Bem Vindo! "+ nome);
+alert(idade + idade2);
 console.log(nome);
 console.log(n1 * n2);
 console.log(frase.toLowerCase());
-//alert(frase.replace("Japão","Brasil"));
+alert(frase.replace("Japão","Brasil"));*/
