404724b8852248e6145bf39989e9350b7401e7b8
Tarcizio Dantas <tarciziomd@gmail.com>
2021-10-05 11:51:43 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 var nome = "Tarcizio Dantas"
-alert("Bem Vindo! "+ nome);
+var n1 = 20;
+var n2 = 30;
+var frase = "Japão é o melhor time do mundo";
+//alert("Bem Vindo! "+ nome);
+//alert(idade + idade2);
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.toLowerCase());
+//alert(frase.replace("Japão","Brasil"));
