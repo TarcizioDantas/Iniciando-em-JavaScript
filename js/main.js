cca93d278e62893a05386ccb9e87de5d52fdf0f5
Tarcizio Dantas <tarciziomd@gmail.com>
2021-10-06 09:52:10 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+
+/*
 var fruta = {nome:"maça", cor:"vermelha"}
 console.log(fruta.nome);
 alert(fruta.cor);
-
+*/
 
 //var lista = ["maçã","pêra","laranja"];
 //lista.push("uva");
