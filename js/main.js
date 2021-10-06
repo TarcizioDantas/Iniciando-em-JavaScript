91c881571ca2ca5f5360e7ef62b5f3e302ae06ad
Tarcizio Dantas <tarciziomd@gmail.com>
2021-10-06 09:48:45 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
-var lista = ["maçã","pêra","laranja"];
+var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+
+
+//var lista = ["maçã","pêra","laranja"];
 //lista.push("uva");
 //lista.pop();
-console.log(lista[0]);
-console.log(lista.toString()[0]);
-console.log(lista.join(" - "));
+//console.log(lista[0]);
+//console.log(lista.toString()[0]);
+//console.log(lista.join(" - "));
 
 
 /*var nome = "Tarcizio Dantas"
