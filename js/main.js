c396cfa8b987a5d0f219428542480b85c2a0750c
Tarcizio Dantas <tarciziomd@gmail.com>
2021-10-09 11:23:42 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,15 +2,25 @@ function soma(n1, n2){
     return n1 + n2;
 }
 
-function setReplace(frase, nome, novo_nome){
-    return frase.replace(nome, novo_nome)
+function validaIdade(idade){
+    var validar = 0;
+    if(idade >= 18){
+        validar = true;
+    }else{
+        validar = false
+    }
+    return validar;
 }
 
+var idade = prompt("Qaul a sua iadade?");
+validaIdade(idade);
+console.log(validar);
+
+/*
 alert(soma(5, 10));
 alert(setReplace("Vai japão", "japão", "Brasil"));
 
 
-/*
 var d = new Date();
 alert(d.getDay);
 alert(d.getHours);
